544db65d0a5553f2ad270dd7b3fba9aaa4ef9021
= <michal.pomaranski@gmail.com>
2015-10-20 20:20:38 +0200

    added removing of a directory with the same day of the week

--- backup.sh
+++ backup.sh
@@ -16,11 +16,22 @@ DESTINATION=~/backup/Studia/
 # backup
 echo "Making a backup of $SOURCE in ${DESTINATION}..."
 date="$(date +'%A-%Y-%m-%d:%H:%M:%S')"
+day_of_week="$(date +'%A')"
 destination_directory="$DESTINATION"
-destination_with_date="$destination_directory$(basename $DESTINATION)_$date"
-echo $destination_with_date
-mkdir -p $destination_with_date
+full_destination="$destination_directory$(basename $DESTINATION)_$date"
+mkdir -p $full_destination
 
-rsync -ra --delete $SOURCE $destination_with_date
-echo "Backup saved in $destination_with_date" 
+rsync -ra --delete $SOURCE $full_destination
+echo "Backup saved in $full_destination" 
+
+# remove all of the other backups made on the same day (except the one made now)
+for i in "$destination_directory$(basename $DESTINATION)_$day_of_week*"; do
+	for file in $i; do
+		if [[ $file != "$full_destination" ]]
+		then
+			rm -r $file
+		fi
+	done
+done
+
 echo "Backup finished."
