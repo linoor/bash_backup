0274a8381ff65d1f6dc33c7d0788dbe8b86014c6
= <michal.pomaranski@gmail.com>
2015-10-20 13:30:50 +0200

    wrote specification

--- backup.sh
+++ backup.sh
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 # Michał Pomarański
 # grupa nr 3
+
+# A script doing a backup of /home folder, keeping up to 7 backups (for each day of the week) as history.
+# Using rsync to perform the backup
+# checking for disk space before attempting the backup.
+# sending an email alert if there's no sufficient disk space.
+# writing a report showing start time, end time and quantity of data copied.
+# offering a choice of tar or rsync.
+# comparing the size of today's backup to yesterdays and sending the user an email of it changed significantly.
