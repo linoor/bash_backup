20919618e784f3e144c2679585957855a0a0a839
= <michal.pomaranski@gmail.com>
2015-10-26 10:32:08 +0100

    fix for running from different directory

--- backup.sh
+++ backup.sh
@@ -15,7 +15,8 @@ użycie:
 	--dest - folder, do którego ma być zrobiony backup.
 "
 
-source ./helper.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+source $DIR/helper.sh
 
 option="$1"
 for option in "$@"
